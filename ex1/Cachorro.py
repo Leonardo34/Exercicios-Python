fb290a7cc1eefc67827cc7ba7f4eac2f6261f449
Leonardo Broch de Morais <leo_broch@hotmail.com>
2016-11-10 15:53:42 -0200

    Adicionado TestaDoge

--- ex1/Cachorro.py
+++ ex1/Cachorro.py
@@ -1,7 +1,11 @@
 class Cachorro:
-
 	patas = 4
+	alimento = 'racao'
 
 	def __init__ (self, nome):
-
 		self.nome = nome
+
+	def diga_ola (self):
+		print 'ola'
+	
+
---
+++ ex1/TestaDog.py
@@ -0,0 +1,4 @@
+from Cachorro import Cachorro
+
+dog = Cachorro('Doge')
+dog.diga_ola()
